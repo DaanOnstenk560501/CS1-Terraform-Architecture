87079b387815d5f65e1769d1e8d2d43a24e214e4
Daan <560501@student.fontys.nl>
2025-09-23 12:10:21 +0200

    Commit 1

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Just one VPC to start
+resource "aws_vpc" "production" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "production-vpc"
+  }
+}
